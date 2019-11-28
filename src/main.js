b3e362b08db992778a03a6c006111ffee4e3d47e
翟宇鑫 <zhaiyuxin@zsyoushi.com>
2019-11-28 16:47:41 +0800

    话题列表

--- src/main.js
+++ src/main.js
@@ -14,10 +14,34 @@ import store from './store'
 import VueSweetalert from './plugins/vue-sweetalert'
 import Message from './plugins/message'
 import './filters'
+import { mockArticles } from './mock/data'
+import ls from './utils/localStorage'
 
 // 使用插件
 Vue.use(VueSweetalert)
 Vue.use(Message)
+
+// eslint-disable-next-line no-unused-vars
+const AddMockData = (() => {
+    // 是否加入测试数据
+    const isAddMockData = true
+    // 用户数据
+    let userArticles = ls.getItem('articles')
+
+    if (Array.isArray(userArticles)) {
+        userArticles = userArticles.filter(article => parseInt(article.uid) === 1)
+    } else {
+        userArticles = []
+    }
+
+    if (isAddMockData) {
+        // 合并用户数据和测试数据，使用合并值作为所有文章
+        store.commit('UPDATE_ARTICLES', [...userArticles, ...mockArticles(10)])
+    } else {
+        // 使用用户数据作为所有文章
+        store.commit('UPDATE_ARTICLES', userArticles)
+    }
+})()
 
 // 设置 false 以阻止 Vue 在启动时生成生产提示
 Vue.config.productionTip = false
