bad7f07e9729a83e6357e28d9d011659e2942832
翟宇鑫 <zhaiyuxin@zsyoushi.com>
2019-12-05 13:39:30 +0800

    github pages

--- vue.config.js
+++ vue.config.js
@@ -2,5 +2,5 @@ module.exports = {
     // 基本路径
     // baseUrl: './',    //vue-cli 3.3 以下版本使用
     // 兼容 github pages
-    // publicPath: 'vuejs-essential/dist', //vue-cli 3.3+ 新版本使用
+    publicPath: 'vuejs-essential/dist', //vue-cli 3.3+ 新版本使用
 };
